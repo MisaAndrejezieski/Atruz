36d85aa71a68c0179f6e1062b6718096ac7c01d7
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-02-25 22:33:23 -0300

    Update app.py

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from models import db
+from routes import init_routes
+
+app = Flask(__name__)
+
+# Configuração do banco SQLite
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../database/altruz.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+init_routes(app)
+
+# Criar tabelas automaticamente
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
